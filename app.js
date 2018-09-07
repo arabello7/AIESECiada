a136e2e0aa2fa5b4de8093687ed1d86c55aabf52
arabello <tomasz.m.jurek@gmail.com>
2018-09-07 12:31:42 +0200

    2 rounds coded

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ teamPoints[1] = 0;
 var queNum = 0;
 var countPress = 0;
 
+//potem do consoleCtrl
 function switchTeam () {
     activeTeam === 0 ? activeTeam = 1 : activeTeam = 0;
 }
@@ -23,19 +24,32 @@ var consoleController = (function() {
     firstAns[0] = 0; //na 2 runde wyzerowanie 
     firstAns[1] = 0;
     
+    function resetScore() {
+        //czy na poczatku tego nie wywolywac zamiast pisania
+        roundScore = 0;
+        teamErrCount[0] = 0;
+        teamErrCount[1] = 0;
+        firstAns[0] = 0; //na 2 runde wyzerowanie 
+        firstAns[1] = 0;
+        countPress = 0;
+    }
+    
     // Zmienia team po 3 blednych
     function changeTeam () {
         // Errors condition
         if (teamErrCount[activeTeam] >= 3) { //4 może jako big error?
             switchTeam();
             return 1; //trigger do lastAns
-        }  //moze funkcja samo change team
+        } 
     }
     
     function chooseTeam () {
-        if (firstAns[0] > firstAns[1]) activeTeam = 0;
+        // Two wrong answers
+        if (firstAns[0] === firstAns[1]) {
+            switchTeam();
+            console.log('pytam teraz na zmiane');
+        } else if (firstAns[0] > firstAns[1]) activeTeam = 0;
         else activeTeam = 1;
-        //*jezeli obaj zle to ten co szybciej :/
     }
     
     return {
@@ -43,22 +57,27 @@ var consoleController = (function() {
         readAnswer : function (queNum, input) {
             // czy to powinno byc ciagle aktywne??, mozna tylko max liczba razy + obsluga wyjscia po wygranej/przegranej
             console.log('answer: ' + input);
-
+            //czy ans razem z input jest potrzebne
             var ans = input;
             var scale = data[queNum].answers.length;
             
+            // After wrong answer
             if (ans === '-1') {
-                // After wrong answer
-                screenController.showOneError();
-                teamErrCount[activeTeam]++;
+                // First wrong answers
+                if (countPress < 2) {
+                    screenController.showBigError();
+                } else {
+                    screenController.showOneError();
+                    teamErrCount[activeTeam]++;
+                }
                 // Last answer is incorrect
                 if (countPress > 99) {
                     switchTeam();
                     console.log('wygrana teamu' + activeTeam);
                     teamPoints[activeTeam] += roundScore;
+                    resetScore();
                     audioController.playJoke();
                 }
-                
                 // Triggers last answer condition
                 if (changeTeam() === 1) {
                     //zmiana teamErrCount, potem do wyswietlania X
@@ -68,13 +87,14 @@ var consoleController = (function() {
             } else if (ans >= 0 && ans <= scale) {
                 // After right answer
                 audioController.playGood();
-                screenController.putAnsOnScreen(0, input);
+                screenController.putAnsOnScreen(queNum, input);
                 roundScore += data[queNum].points[input];
                 screenController.updateRoundScore(roundScore);
                 // Last answer is correct
                 if (countPress > 99) {
                     console.log('wygrana teamu ' + activeTeam);
                     teamPoints[activeTeam] += roundScore;
+                    resetScore();
                     audioController.playJoke();
                 }
             } else {
@@ -106,8 +126,6 @@ var consoleController = (function() {
                  console.log(i + '. ' + data[queNum].answers[i]);
             }
             console.log('-1. Żadna z powyższych');
-            // Funkcja wczytująca pole tekstowe
-//            readAnswer(queNum); 
         }
     }
 })();
@@ -124,8 +142,8 @@ var dataController = (function () {
     
     return {
         loadData : function () {
-            data[0] = new Data('Przedmiot szkolny, który najmniej przydaje się w życiu?', ['Kanapka', 'Piórnik','Olówek', 'Dziennik'], [47, 23, 15, 9]);
-            data[1] = new Data('Więcej niż jedno zwierzę to?', ['Lama', 'Owca'], [99, 1]);
+            data[0] = new Data('Przedmiot szkolny, który najmniej przydaje się w życiu?', ['Kanapka', 'Piórnik','Olówek', 'Dziennik', 'Plastus'], [47, 23, 15, 9, 1]);
+            data[1] = new Data('Więcej niż jedno zwierzę to?', ['Lama', 'Owca', 'Krowa', 'Stado', 'Klucz'], [99, 67, 43, 15, 14]);
         }
     }
 })();
@@ -149,10 +167,11 @@ var screenController = (function() {
                 document.querySelector('.wrong-0-' + i).textContent = '';
                 document.querySelector('.wrong-1-' + i).textContent = '';
             }
-
-            for (var i = 0; i < 5; i ++) {
+            
+            for (var i = 0; i < data[queNum].answers.length; i ++) {
                 document.querySelector('#ans-pts-' + i).textContent = 0;
                 document.querySelector('#ans-pts-' + i).style.display = 'block';
+                document.querySelector('#ans-' + i).textContent = "";
             }
             
             document.querySelector('#round-pts').textContent = 'SUM 0';
@@ -186,8 +205,11 @@ var screenController = (function() {
         },
         // Used after beginning of round, potem raczej duże X
         resetErrors : function () {
-            document.querySelector('.wrong-0-0').textContent = "";
-            document.querySelector('.wrong-1-0').textContent = "";
+            //nie tak szybko
+            document.querySelector('.wrong-0-1').textContent = "";
+            document.querySelector('.wrong-1-1').textContent = "";
+//            document.querySelector('.wrong-0-0').textContent = "";
+//            document.querySelector('.wrong-1-0').textContent = "";
         }
     }
 })();
@@ -224,13 +246,13 @@ document.querySelector('#btn-2').addEventListener('click', function(){
 document.querySelector('#btn-3').addEventListener('click', function(){
     answerButton(3);
 });
-document.querySelector('#btn-4').addEventListener('click', function() {
+document.querySelector('#btn-4').addEventListener('click', function(){
     countPress++;
     consoleController.readAnswer(queNum,'-1');
 });
 
 // Function executed after clicking button
-function answerButton (num){
+function answerButton(num){
     countPress++;
     consoleController.readAnswer(queNum, num); 
     document.querySelector('#btn-' + num).style.display = 'none';
@@ -245,15 +267,33 @@ function answerButton (num){
 //    consoleController.printAnswers(0);
 //}
 
-//function roundOne () {
-//    //
-//}
+
+//4 potem na liczbe odpowiedzi odczytywana
+function bringTheButtons(){
+    for (var i = 0; i < data[queNum].answers.length; i++) {
+         document.querySelector('#btn-' + i).style.display = 'block';
+    }
+}
 
 //TESTING
-console.log('active: ' + activeTeam);
-consoleController.printAnswers(queNum);
+function roundOne(){
+    queNum = 0;
+    console.log('active: ' + activeTeam);
+    consoleController.printAnswers(queNum);
+}
 
-function showPoints () {
+function roundTwo(){
+    screenController.resetRound();
+    bringTheButtons();
+    //roundScore = 0;
+//    countPress = 0;
+    queNum = 1;
+    console.log('active: ' + activeTeam);
+    consoleController.printAnswers(queNum);
+}
+
+
+function showPoints(){
     console.log('Team 0: ' + teamPoints[0]);
     console.log('Team 1: ' + teamPoints[1]);
 }
